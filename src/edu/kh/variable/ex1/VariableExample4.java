6c0a215d3a0eab580fef749d61520359a5bb1104
LJAEH <wogurdlek33@gmail.com>
2022-12-07 10:34:08 +0900

    오타수정

--- src/edu/kh/variable/ex1/VariableExample4.java
+++ src/edu/kh/variable/ex1/VariableExample4.java
@@ -4,7 +4,7 @@ public class VariableExample4 {
 	public static void main(String[] args) {
 		
 		/* 강제형변환
-		 * - 기존 자료형을 원하는 잘형으로 강제 변환시키는 것
+		 * - 기존 자료형을 원하는 자료형으로 강제 변환시키는 것
 		 * 
 		 * 1) 값의 범위가 큰 자료형을 작은 자료형으로 변환할 때 사용
 		 * 2) 출력되는 데이터의 표기법을 변화시키고 싶을 때 
